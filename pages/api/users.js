4a4de8155dbebfefcb70fd497bea05677bc27758
Vương Đức Tuấn <vuongductuanktmt@gmail.com>
2020-05-01 11:17:07 +0700

    fix bug

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,9 +1,9 @@
 // Fake users data
 const users = [
   { id: 1, fullName: 'VTD' },
-  { id: 2, fullName: 'VTD' },
-  { id: 3, fullName: 'VTD' }, 
-  { id: 4, fullName: 'VTD'}]
+  { id: 2, fullName: 'VDT' },
+  { id: 3, fullName: 'VTH' }, 
+  { id: 4, fullName: 'VTP'}]
 
 export default (req, res) => {
   // Get data from your database
